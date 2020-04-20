3b0e16d91314ede74f2d7d43615660bc4df7d1f5
AminDiyasSerikuly <160106014@stu.sdu.edu.kz>
2020-04-21 02:46:16 +0600

    fix bug with packet rest price calculation

--- app/Models/UserPacket.php
+++ app/Models/UserPacket.php
@@ -9,9 +9,14 @@ use DB;
 
 class UserPacket extends Model
 {
+
+
     protected $table = 'user_packet';
     protected $primaryKey = 'user_packet_id';
 
     use SoftDeletes;
     protected $dates = ['deleted_at'];
+
+
+
 }
