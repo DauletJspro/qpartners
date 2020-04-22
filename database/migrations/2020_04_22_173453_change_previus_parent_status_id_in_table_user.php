19064a13d7895c4ce2311f3413fa1ea2e2014adc
AminDiyasSerikuly <160106014@stu.sdu.edu.kz>
2020-04-22 19:04:44 +0600

    change status id

--- database/migrations/2020_04_22_173453_change_previus_parent_status_id_in_table_user.php
+++ database/migrations/2020_04_22_173453_change_previus_parent_status_id_in_table_user.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class ChangeStatusId extends Migration
+class ChangePreviusParentStatusIdInTableUser extends Migration
 {
     /**
      * Run the migrations.
@@ -15,20 +15,6 @@ class ChangeStatusId extends Migration
      */
     public function up()
     {
-        $diamondStatuses = UserStatus::where(['user_status_id' => 28])->first();
-        $diamondStatuses->user_status_id = 29;
-        $diamondStatuses->sort_num = 108;
-        $diamondStatuses->save();
-
-        $userStatuses = new UserStatus();
-        $userStatuses->user_status_id = 28;
-        $userStatuses->user_status_name = 'Сапфировый директор';
-        $userStatuses->user_status_money = 0;
-        $userStatuses->sort_num = 107;
-        $userStatuses->is_show = 1;
-        $userStatuses->save();
-
-
         $parents = Users::all();
         foreach ($parents as $parent) {
             $parentFollowers = Users::parentFollowers($parent->user_id);
