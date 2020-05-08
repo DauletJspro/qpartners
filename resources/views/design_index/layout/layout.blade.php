b81407d725e0f2ed073632341d6bce229ea758a2
AminDiyasSerikuly <160106014@stu.sdu.edu.kz>
2020-05-08 23:22:38 +0600

    add new design

--- resources/views/design_index/layout/layout.blade.php
+++ resources/views/design_index/layout/layout.blade.php
@@ -51,48 +51,48 @@
 <!-- include jQuery -->
 <script src="/new_design/js/jquery.main.js"></script>
 <!-- include notify -->
-<script src="/notify/notify.js"></script>
-<script src="/notify/notify.min.js"></script>
+{{--<script src="/notify/notify.js"></script>--}}
+{{--<script src="/notify/notify.min.js"></script>--}}
 
 
 @yield('js')
-<script>
-    function addItemToBasket(tag_object) {
-        var method = $(tag_object).data('method');
-        var item_id = $(tag_object).data('item-id');
-        var user_id = $(tag_object).data('user-id');
-        ajax(method, item_id, user_id);
-    }
+{{--<script>--}}
+{{--    function addItemToBasket(tag_object) {--}}
+{{--        var method = $(tag_object).data('method');--}}
+{{--        var item_id = $(tag_object).data('item-id');--}}
+{{--        var user_id = $(tag_object).data('user-id');--}}
+{{--        ajax(method, item_id, user_id);--}}
+{{--    }--}}
 
-    
-    function ajax(method, item_id, user_id) {
-        var CSRF_TOKEN = $('meta[name="csrf-token"]').attr('content');
-        $.ajax({
-            url: "{{route('basket.isAjax')}}",
-            type: "POST",
-            data: {
-                _token: CSRF_TOKEN,
-                method_name: method,
-                item_id: item_id,
-                user_id: user_id,
-            },
-            success: function (data) {
-                if (method == 'delete') {
-                    $("#product-section").load(location.href + " #product-section");
-                    $("#total-price-div").load(location.href + " #total-price-div");
-                } else if (data.method == 'add') {
-                    if (data.success == 1) {
-                        $.notify(data.message, "success");
-                    } else if (data.success == -1) {
-                        $.notify(data.message, "error");
-                    } else if (data.success == 0) {
-                        $.notify(data.message, "error");
-                    }
-                }
-            }
-        });
-    }
-</script>
+{{--    --}}
+{{--    function ajax(method, item_id, user_id) {--}}
+{{--        var CSRF_TOKEN = $('meta[name="csrf-token"]').attr('content');--}}
+{{--        $.ajax({--}}
+{{--            url: "{{route('basket.isAjax')}}",--}}
+{{--            type: "POST",--}}
+{{--            data: {--}}
+{{--                _token: CSRF_TOKEN,--}}
+{{--                method_name: method,--}}
+{{--                item_id: item_id,--}}
+{{--                user_id: user_id,--}}
+{{--            },--}}
+{{--            success: function (data) {--}}
+{{--                if (method == 'delete') {--}}
+{{--                    $("#product-section").load(location.href + " #product-section");--}}
+{{--                    $("#total-price-div").load(location.href + " #total-price-div");--}}
+{{--                } else if (data.method == 'add') {--}}
+{{--                    if (data.success == 1) {--}}
+{{--                        $.notify(data.message, "success");--}}
+{{--                    } else if (data.success == -1) {--}}
+{{--                        $.notify(data.message, "error");--}}
+{{--                    } else if (data.success == 0) {--}}
+{{--                        $.notify(data.message, "error");--}}
+{{--                    }--}}
+{{--                }--}}
+{{--            }--}}
+{{--        });--}}
+{{--    }--}}
+{{--</script>--}}
 
 </body>
 </html>
