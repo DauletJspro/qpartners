e35206fe464d1f3b5e07310d9a477228a3ce6d16
AminDiyasSerikuly <160106014@stu.sdu.edu.kz>
2020-05-09 04:16:32 +0600

    fixed untracked files

--- storage/framework/views/36ae0b7de5a8d129a8d2261841f9401f2c4705e2.php
+++
@@ -1,3 +0,0 @@
-<div class="quadrate white-square" onclick="showCallFriendModal('<?php echo e(URL('/')); ?>','<?php echo e($parent_id); ?>','<?php echo e($is_left); ?>')">
-    <div class="user_id"></div>
-</div>
