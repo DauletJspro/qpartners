3b2945b9e336436e7621407d80b0b6173f4bce6c
AminDiyasSerikuly <160106014@stu.sdu.edu.kz>
2020-05-15 15:03:43 +0600

    add new leaders and administrators

--- resources/views/design_index/representative/show.blade.php
+++ resources/views/design_index/representative/show.blade.php
@@ -1,63 +1,63 @@
-@extends('design_index.layout.layout')
-@section('meta-tags')
+{{--@extends('design_index.layout.layout')--}}
+{{--@section('meta-tags')--}}
 
-    <title>Qpartners Shop</title>
-    <meta name="description"
-          content="«Qpartners» - это уникальный медиа проект с широким набором возожностей для взаймодествия с участниками виртуального рынка"/>
-    <meta name="keywords" content="Qpartners"/>
+{{--    <title>Qpartners Shop</title>--}}
+{{--    <meta name="description"--}}
+{{--          content="«Qpartners» - это уникальный медиа проект с широким набором возожностей для взаймодествия с участниками виртуального рынка"/>--}}
+{{--    <meta name="keywords" content="Qpartners"/>--}}
 
-@endsection
+{{--@endsection--}}
 
 
-@section('content')
-    <main id="mt-main">
-        <!-- Mt Content Banner of the Page -->
-        <section class="mt-contact-banner">
-            <div class="container">
-                <div class="row">
-                    <div class="col-xs-12 text-center">
-                        <h1>СПИСОК ПРЕДСТАВИТЕЛЕЙ</h1>
-                        <nav class="breadcrumbs">
-                            <ul class="list-unstyled">
-                            </ul>
-                        </nav>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <!-- Mt Content Banner of the Page end -->
-        <!-- Mt About Section of the Page -->
-        <section class="mt-about-sec" style="padding-bottom: 0;">
-            <div class="container">
-                <div class="row">
-                    <div class="col-xs-12">
-                        <div class="txt wow fadeInUp" data-wow-delay="0.4s">
-                            <h2>СПИСОК ПРЕДСТАВИТЕЛЕЙ С КОНТАКТАМИ</h2>
-                            <p>
-                                В нижеследующем списке Вы можете ознакомиться с представителями компании Qyran Partners
-                                Club
-                                <br>
-                                Для каждого региона назначается свой представитель компании
-                                <br>
-                                Если у вас есть вопросы то Вы можете обратиться в представителю в вашем регионе
-                                <br>
-                                Контактные данные представителя Вашего региона Вы можете найти в этом списке
-                                <br>
-                                <br>
-                                Если Вы в списке не нашли представителя своего региона, то Вы можете стать первым
-                                представителем компании в своем регионе
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <!-- Mt About Section of the Page -->
-        <div class="container faq-section mt-about-sec">
-            <div class="row txt">
-                <div class="col-xs-12">
-                    <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-                        <?php $counter = 0; ?>
+{{--@section('content')--}}
+{{--    <main id="mt-main">--}}
+{{--        <!-- Mt Content Banner of the Page -->--}}
+{{--        <section class="mt-contact-banner">--}}
+{{--            <div class="container">--}}
+{{--                <div class="row">--}}
+{{--                    <div class="col-xs-12 text-center">--}}
+{{--                        <h1>СПИСОК ПРЕДСТАВИТЕЛЕЙ</h1>--}}
+{{--                        <nav class="breadcrumbs">--}}
+{{--                            <ul class="list-unstyled">--}}
+{{--                            </ul>--}}
+{{--                        </nav>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </section>--}}
+{{--        <!-- Mt Content Banner of the Page end -->--}}
+{{--        <!-- Mt About Section of the Page -->--}}
+{{--        <section class="mt-about-sec" style="padding-bottom: 0;">--}}
+{{--            <div class="container">--}}
+{{--                <div class="row">--}}
+{{--                    <div class="col-xs-12">--}}
+{{--                        <div class="txt wow fadeInUp" data-wow-delay="0.4s">--}}
+{{--                            <h2>СПИСОК ПРЕДСТАВИТЕЛЕЙ С КОНТАКТАМИ</h2>--}}
+{{--                            <p>--}}
+{{--                                В нижеследующем списке Вы можете ознакомиться с представителями компании Qyran Partners--}}
+{{--                                Club--}}
+{{--                                <br>--}}
+{{--                                Для каждого региона назначается свой представитель компании--}}
+{{--                                <br>--}}
+{{--                                Если у вас есть вопросы то Вы можете обратиться в представителю в вашем регионе--}}
+{{--                                <br>--}}
+{{--                                Контактные данные представителя Вашего региона Вы можете найти в этом списке--}}
+{{--                                <br>--}}
+{{--                                <br>--}}
+{{--                                Если Вы в списке не нашли представителя своего региона, то Вы можете стать первым--}}
+{{--                                представителем компании в своем регионе--}}
+{{--                            </p>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </section>--}}
+{{--        <!-- Mt About Section of the Page -->--}}
+{{--        <div class="container faq-section mt-about-sec">--}}
+{{--            <div class="row txt">--}}
+{{--                <div class="col-xs-12">--}}
+{{--                    <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">--}}
+{{--                        <?php $counter = 0; ?>--}}
 {{--                        @foreach($cities as $city)--}}
 {{--                            <?php $counter++; ?>--}}
 {{--                            <div class="panel panel-default">--}}
@@ -117,10 +117,10 @@
 {{--                                </div>--}}
 {{--                            </div>--}}
 {{--                        @endforeach--}}
-                    </div>
-                </div>
-            </div>
-        </div>
-    </main>
-@endsection
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </main>--}}
+{{--@endsection--}}
 
