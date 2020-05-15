017757b1c03da80fb564bd43cd61d594ee49da57
AminDiyasSerikuly <160106014@stu.sdu.edu.kz>
2020-05-15 15:09:19 +0600

    add new leaders and administrators

--- resources/views/design_index/representative/show-1.blade.php
+++ resources/views/design_index/representative/show-1.blade.php
@@ -52,5 +52,73 @@
                 </div>
             </div>
         </section>
+        <div class="container faq-section mt-about-sec">
+            <div class="row txt">
+                <div class="col-xs-12">
+                    <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+                        <?php $counter = 0; ?>
+                        @foreach($cities as $city)
+                            <?php $counter++; ?>
+                            <div class="panel panel-default">
+                                <div class="panel-heading" role="tab" id="heading{{$city->city_id}}">
+                                    <h4 class="panel-title">
+                                        <a class="collapsed" role="button" data-toggle="collapse"
+                                           data-parent="#accordion"
+                                           href="#collapse{{$city->city_id}}" aria-expanded="false"
+                                           aria-controls="collapse{{$city->city_id}}">
+                                            {{ sprintf('%s &emsp; г. %s', $counter,$city->city_name_ru)}}
+                                        </a>
+                                    </h4>
+                                </div>
+                                <div id="collapse{{$city->city_id}}" class="panel-collapse collapse" role="tabpanel"
+                                     aria-labelledby="heading{{$city->city_id}}">
+                                    <div class="panel-body">
+                                        <?php
+                                        $subCounter = 0;
+                                        $representatives = \App\Models\Representative::where(['city_id' => $city->city_id])->get();
+                                        ?>
+                                        @foreach($representatives as $representative)
+                                            <?php $subCounter++; ?>
+                                            <div class="container" style="margin-top: 1rem;">
+                                                <div class="row">
+                                                    <div class="col-sm-4">
+                                                        <strong class="representative">{{$subCounter .'&emsp;'.  $representative->full_name}}</strong>
+                                                    </div>
+                                                    <div class="col-sm-4">
+                                                        <strong class="representative">{{'г. ' .  $representative->city ? $representative->city->city_name_ru : 'NULL'}}</strong>
+                                                    </div>
+                                                    <div class="col-sm-4">
+                                                        <ul style="list-style: none;">
+                                                            <li>
+                                                                <strong class="representative">
+                                                                    <i class="fa fa-phone"></i>
+                                                                    &emsp; {{$representative->phone_number}}
+                                                                </strong>
+                                                            </li>
+                                                            <li>
+                                                                <strong class="representative">
+                                                                    <i class="fa fa-whatsapp"></i>
+                                                                    &emsp;{{$representative->whatsapp}}
+                                                                </strong>
+                                                            </li>
+                                                            <li>
+                                                                <strong class="representative">
+                                                                    <i class="fa fa-instagram"></i>
+                                                                    &emsp;{{$representative->instagram}}
+                                                                </strong>
+                                                            </li>
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        @endforeach
+                                    </div>
+                                </div>
+                            </div>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
     </main>
 @endsection
