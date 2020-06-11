9951f690e01a21d9a64a2e77617e89f7e43019ae
AminDiyasSerikuly <160106014@stu.sdu.edu.kz>
2020-06-11 11:05:15 +0600

    update cron in Kernel file

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('globalBonus:everyMonth')
-            ->everyMinute();
+            ->cron('0 0 1 * *');
     }
 
     /**
