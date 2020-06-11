9107a1985b82df085d03fea4584ad40d8b59deba
AminDiyasSerikuly <160106014@stu.sdu.edu.kz>
2020-06-11 19:50:52 +0600

    add new presentation

--- resources/views/admin/presentation/presentation.blade.php
+++ resources/views/admin/presentation/presentation.blade.php
@@ -17,18 +17,19 @@
                             <div class="nav-tabs-custom">
                                 <ul class="nav nav-tabs">
                                     <li class="active">
-                                        <a href="#qaz" data-toggle="tab">Қазақша</a>
+                                        <a href="#qaz" data-toggle="tab">Маркетинг план</a>
                                     </li>
                                     <li>
-                                        <a href="#rus" data-toggle="tab">Русский</a>
+                                        <a href="#rus" data-toggle="tab">
+                                            Каталог продукции</a>
                                     </li>
                                 </ul>
                                 <div class="tab-content" style="min-height: 400px">
                                     <div class="active tab-pane" id="qaz">
-                                        <iframe style="border:1px solid #666CCC" title="PDF in an i-Frame" src="/presentation/qaz.pdf?v=1" frameborder="1" scrolling="auto" height="800" width="100%" ></iframe>
+                                        <iframe style="border:1px solid #666CCC" title="PDF in an i-Frame" src="/presentation/marketing_plan.pdf?v=1" frameborder="1" scrolling="auto" height="800" width="100%" ></iframe>
                                     </div>
                                     <div class="tab-pane" id="rus">
-                                        <iframe style="border:1px solid #666CCC" title="PDF in an i-Frame" src="/presentation/rus.pdf?v=1" frameborder="1" scrolling="auto" height="800" width="100%" ></iframe>
+                                        <iframe style="border:1px solid #666CCC" title="PDF in an i-Frame" src="/presentation/normal_product.pdf?v=1" frameborder="1" scrolling="auto" height="800" width="100%" ></iframe>
                                     </div>
                                 </div>
                             </div>
