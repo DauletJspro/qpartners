ec3a88b9cd3650dcdf33f11dff85b94475a67629
AminDiyasSerikuly <amindiyass@gmail.com>
2020-12-07 12:49:26 +0600

    update price

--- database/seeds/ChangePacketPriceSeeder.php
+++ database/seeds/ChangePacketPriceSeeder.php
@@ -14,13 +14,16 @@ class ChangePacketPriceSeeder extends Seeder
     {
         DB::table('packet')->where(['packet_id' => \App\Models\Packet::CLASSIC])
             ->update([
-                'packet_price' => 50,
+                'packet_price' => 60,
             ]);
         DB::table('packet')->where(['packet_id' => \App\Models\Packet::PREMIUM])->update([
-            'packet_price' => 100,
+            'packet_price' => 120,
         ]);
         DB::table('packet')->where(['packet_id' => \App\Models\Packet::VIP2])->update([
-            'packet_price' => 150,
+            'packet_price' => 180,
+        ]);
+        DB::table('packet')->where(['packet_id' => \App\Models\Packet::GAP])->update([
+            'packet_price' => 300,
         ]);
     }
 }
