572efe6a3314954283b0f3865da56cba51054913
AminDiyasSerikuly <amindiyass@gmail.com>
2021-02-24 18:43:48 +0600

    seed to change password before testing

--- database/seeds/PrepareDataBaseForTestingSeeder.php
+++ database/seeds/PrepareDataBaseForTestingSeeder.php
@@ -14,15 +14,15 @@ class PrepareDataBaseForTestingSeeder extends Seeder
     {
         $users = \App\Models\Users::whereNotNull('user_id')->get();
         foreach ($users as $user) {
-            $user->user_money = 0;
+//            $user->user_money = 0;
             $user->password = Hash::make('123456');
-            $user->status_id = null;
-            $user->pv_balance = 0;
-            $user->gv_balance = 0;
-            $user->sv_balance = 0;
-            $user->gap_status = null;
-            $user->last_status = 0;
-            $user->product_balance = 0;
+//            $user->status_id = null;
+//            $user->pv_balance = 0;
+//            $user->gv_balance = 0;
+//            $user->sv_balance = 0;
+//            $user->gap_status = null;
+//            $user->last_status = 0;
+//            $user->product_balance = 0;
             $user->save();
         }
     }
