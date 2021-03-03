2cfdfb2ae94392f3cc8b5ccc59e97a7e4a8705db
daulet <sailauovdaulet15@gmail.com>
2021-03-03 14:28:11 +0500

    fix bug

--- resources/views/admin/profile/user_packet_list_loop.blade.php
+++ resources/views/admin/profile/user_packet_list_loop.blade.php
@@ -1,19 +1,23 @@
-@foreach($row->packet as $key => $item)
+@php
+$userPacket = \App\Models\UserPacket::where('user_id', '=', \Illuminate\Support\Facades\Auth::user()->user_id)->get()
+@endphp
+
+@foreach($userPacket as $key => $item)
 
     <div class="col-lg-4 col-xs-6">
         <!-- small box -->
-        <div class="small-box" style="background-color: #{{$item->packet_css_color}}">
+        <div class="small-box" style="background-color: #{{$item->packet->packet_css_color}}">
             <div class="inner">
-                <h3 style="font-family: cursive; font-size: 24px">{{$item->packet_name_ru}}</h3>
-                <h4 style="font-size: 22px">{{$item->packet_price *(500/600)}} PV</h4>
+                <h3 style="font-family: cursive; font-size: 24px">{{$item->packet->packet_name_ru}}</h3>
+                <h4 style="font-size: 22px">{{$item->packet->packet_price *(500/600)}} PV</h4>
             </div>
             <div class="icon">
                 <i class="ion ion-bag" style="font-size: 17px"></i>
             </div>
 
-                @if($item->is_active > 0)
+                @if($item->is_active == 0)
                     <a class="small-box-footer" style="font-size: 18px">Приобретенный</a>
-                @else
+                @elseif($item->is_active == 1)
                     <a class="small-box-footer" style="font-size: 18px">Отправил запрос</a>
                 @endif
 
