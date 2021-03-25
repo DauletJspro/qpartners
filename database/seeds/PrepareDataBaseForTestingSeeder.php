074ce142e95022c1de392208b2616c0ca77ac0ad
AminDiyasSerikuly <amindiyass@gmail.com>
2021-03-25 19:19:21 +0600

    fix bug

--- database/seeds/PrepareDataBaseForTestingSeeder.php
+++ database/seeds/PrepareDataBaseForTestingSeeder.php
@@ -14,44 +14,44 @@ class PrepareDataBaseForTestingSeeder extends Seeder
     public function run()
     {
         ## Reset users balance and user packets
-//        $users = \App\Models\Users::whereNotNull('user_id')->get();
-//        foreach ($users as $user) {
-//            $user->user_money = 0;
-//            $user->status_id = null;
-//            $user->pv_balance = 0;
-//            $user->gv_balance = 0;
-//            $user->sv_balance = 0;
-//            $user->gap_status = null;
-//            $user->last_status = 0;
-//            $user->product_balance = 0;
-//            $user->save();
-//        }
-//
-//        $userPackets = \App\Models\UserPacket::where(['is_active' => true])->get();
-//
-//        foreach ($userPackets as $user_packet) {
-//            $user_packet->is_active = false;
-//            $user_packet->save();
-//        }
+        $users = \App\Models\Users::whereNotNull('user_id')->get();
+        foreach ($users as $user) {
+            $user->user_money = 0;
+            $user->status_id = null;
+            $user->pv_balance = 0;
+            $user->gv_balance = 0;
+            $user->sv_balance = 0;
+            $user->gap_status = null;
+            $user->last_status = 0;
+            $user->product_balance = 0;
+            $user->save();
+        }
+
+        $userPackets = \App\Models\UserPacket::where(['is_active' => true])->get();
+
+        foreach ($userPackets as $user_packet) {
+            $user_packet->is_active = false;
+            $user_packet->save();
+        }
 
         ## GAP TO GAPHome
 
-//        $gapUserPackets = \App\Models\UserPacket::where('packet_id', \App\Models\Packet::GAP)->get();
-//
-//        foreach ($gapUserPackets as $userPacket) {
-//            $userPacket->packet_id = \App\Models\Packet::GAPHome;
-//            $userPacket->save();
-//        }
-//
-//        $gapHomeUserPackets = \App\Models\UserPacket::where('packet_id', \App\Models\Packet::GAPHome)->get();
-//        foreach ($gapHomeUserPackets as $gapHomeUserPacket) {
-//            $gapHomeUserPacket->packet_price = 300;
-//            $gapHomeUserPacket->save();
-//        }
+        $gapUserPackets = \App\Models\UserPacket::where('packet_id', \App\Models\Packet::GAP)->get();
+
+        foreach ($gapUserPackets as $userPacket) {
+            $userPacket->packet_id = \App\Models\Packet::GAPHome;
+            $userPacket->save();
+        }
+
+        $gapHomeUserPackets = \App\Models\UserPacket::where('packet_id', \App\Models\Packet::GAPHome)->get();
+        foreach ($gapHomeUserPackets as $gapHomeUserPacket) {
+            $gapHomeUserPacket->packet_price = 300;
+            $gapHomeUserPacket->save();
+        }
 
         ## clear user operation database
-//        DB::table('user_operation')->truncate();
-//
+        DB::table('user_operation')->truncate();
+
         $passiveUsersArray = [
             'Iskak888' => \App\Models\Packet::BASPANA,
             'Mehrinisa888' => \App\Models\Packet::BASPANA,
@@ -81,28 +81,28 @@ class PrepareDataBaseForTestingSeeder extends Seeder
 
 
         ## add new packets to passive users
-        foreach ($passiveUsers as $user) {
-            $packet_id = $passiveUsersArray[$user->login];
-            $packet = \App\Models\Packet::find($packet_id);
-            $userPacket = new \App\Models\UserPacket();
-            $userPacket->user_id = $user->user_id;
-            $userPacket->packet_id = $packet_id;
-            $userPacket->packet_price = $packet->packet_price;
-            $userPacket->created_at = date('Y-m-d H:i:s');
-            $userPacket->updated_at = date('Y-m-d H:i:s');
-            $userPacket->level = 1;
-            $userPacket->is_active = 0;
-            $userPacket->is_paid = 0;
-            $userPacket->is_portfolio = 0;
-            $userPacket->is_epay = 0;
-            $userPacket->user_packet_type = 'item';
-            $userPacket->packet_type = 1;
-            $userPacket->save();
-
-            $user->status_id = $packet->packet_status_id;
-            $user->save();
-
-        }
+//        foreach ($passiveUsers as $user) {
+//            $packet_id = $passiveUsersArray[$user->login];
+//            $packet = \App\Models\Packet::find($packet_id);
+//            $userPacket = new \App\Models\UserPacket();
+//            $userPacket->user_id = $user->user_id;
+//            $userPacket->packet_id = $packet_id;
+//            $userPacket->packet_price = $packet->packet_price;
+//            $userPacket->created_at = date('Y-m-d H:i:s');
+//            $userPacket->updated_at = date('Y-m-d H:i:s');
+//            $userPacket->level = 1;
+//            $userPacket->is_active = 0;
+//            $userPacket->is_paid = 0;
+//            $userPacket->is_portfolio = 0;
+//            $userPacket->is_epay = 0;
+//            $userPacket->user_packet_type = 'item';
+//            $userPacket->packet_type = 1;
+//            $userPacket->save();
+//
+//            $user->status_id = $packet->packet_status_id;
+//            $user->save();
+//
+//        }
 
 
     }
