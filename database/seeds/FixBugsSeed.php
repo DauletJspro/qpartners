51790778d7b37925c4a4b765f8b1aeccaea87834
daulet <sailauovdaulet15@gmail.com>
2021-04-02 16:19:17 +0500

    front-end models

--- database/seeds/FixBugsSeed.php
+++ database/seeds/FixBugsSeed.php
@@ -12,8 +12,18 @@ class FixBugsSeed extends Seeder
      */
     public function run()
     {
-        DB::table('user_packet')->where('user_id', '100165')->whereIn('packet_id', [27,33])->update([
+        DB::table('user_packet')->where('user_id', '=', 100218)->where('packet_id', 23)->update([
             'is_active' => false
         ]);
+
+        DB::table('user_operation')->where('author_id', '=', 100218)->delete();
+
+        $user = \App\Models\Users::where('user_id', '=', '50')->first();
+            $user->user_money = $user->user_money - 96;
+            $user->save();
+
+        $user2 = \App\Models\Users::where('user_id', '=', '9')->first();
+        $user2->user_money = $user2->user_money - 9;
+        $user2->save();
     }
 }
