d3c4b717319efa4fb2e79a72cf1aab00b4b9be9b
daulet <sailauovdaulet15@gmail.com>
2021-04-10 16:02:03 +0500

    seed fix bugs

--- database/seeds/FixBugsSeed.php
+++ database/seeds/FixBugsSeed.php
@@ -13,16 +13,28 @@ class FixBugsSeed extends Seeder
     public function run()
     {
         {
-            DB::table('user_packet')->where('user_id', '=', 100082)->where('packet_id', '=', 27)->update([
-                'is_active' => true
+            DB::table('user_packet')->where('user_id', '=', 100267)->where('packet_id', '=', 31)->update([
+                'is_active' => false
+            ]);
+            
+            DB::table('user_packet'->where('user_id', '=', 100274)->where('packet_id', '=', 33)->update([
+            	'is_active' => false
             ]);
 //
 //            DB::table('user_operation')->where('author_id', '=', 100082)->where('recipient_id', '=', 100082)
 //                ->whereDate('created_at', '2021-04-07')->delete();
 //
-//            $user = \App\Models\Users::where('user_id', '=', 100082)->first();
-//            $user->personal_sv_balance = $user->personal_sv_balance - 5;
-//            $user->save();
+            $user = \App\Models\Users::where('user_id', '=', 100267)->first();
+            $user->personal_sv_balance = $user->personal_sv_balance - 1;
+            $user->save();
+		
+		    $user1 = \App\Models\Users::where('user_id', '=', 100263)->first();
+		    $user1->group_sv_balance = $user1->group_sv_balance - 6;
+		    $user1->save();
+		
+		    $user2 = \App\Models\Users::where('user_id', '=', 100274)->first();
+		    $user2->group_sv_balance = $user2->group_sv_balance - 5;
+		    $user2->save();
 
 //            $users1 = \App\Models\Users::whereIn('user_id',  [100124, 100103, 100088, 100052, 100048, 100047, 46,
 // 11])->get();
