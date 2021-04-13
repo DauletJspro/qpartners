87c68f49c46ae61d54d396c96b16dbbda36a8b4e
daulet <sailauovdaulet15@gmail.com>
2021-04-13 09:58:01 +0500

    fix bug

--- database/seeds/AddPVandPSV.php
+++ database/seeds/AddPVandPSV.php
@@ -14,8 +14,7 @@ class AddPVandPSV extends Seeder
         $users = \App\Models\Users::whereIn('user_id', [15, 13, 20, 46, 224, 226, 228, 100002, 100047, 100048, 100051,
 	        100080, 100139, 100176, 100177, 100178, 100186, 100193, 100210, 9])->get();
         foreach ($users as $user){
-        	$user->pv_balance = $user->pv_balance + 100;
-        	$user->personal_sv_balance = $user->personal_sv_balance + 1;
+        	$user->personal_sv_balance = $user->personal_sv_balance + 4;
         	$user->save();
         }
     }
