c460727f67b3d8090bd2b00f41f485fc4904de25
daulet <sailauovdaulet15@gmail.com>
2021-04-13 09:49:46 +0500

    Add pv and psv for promo

---
+++ database/seeds/AddPVandPSV.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AddPVandPSV extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = \App\Models\Users::whereIn('user_id', [15, 13, 20, 46, 224, 226, 228, 100002, 100047, 100048, 100051,
+	        100080, 100139, 100176, 100177, 100178, 100186, 100193, 100210, 9])->get();
+        foreach ($users as $user){
+        	$user->pv_balance = $user->pv_balance + 100;
+        	$user->personal_sv_balance = $user->personal_sv_balance + 1;
+        	$user->save();
+        }
+    }
+}
