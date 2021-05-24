3389d5037fc42075be325ffb18252315ca367ef2
Yerlan <cool.krutoi@inbox.ru>
2021-05-24 11:55:23 +0600

    PayCashbackController changes

--- app/Http/Controllers/Admin/PayCashbackController.php
+++ app/Http/Controllers/Admin/PayCashbackController.php
@@ -16,29 +16,6 @@ class PayCashbackController extends Controller
 
     public function create(Request $request)
     {
-//        if(!(Auth::user()->role_id == Role::CLIENT)){
-//            $user = Auth::user();
-//
-//            $product = Product::findOrFail($request->product_id);
-//            $order = new Order();
-//            $order_code = time();
-//            $price = ($product->product_price * $request->product_count ) * \App\Models\Currency::pvToKzt();
-//
-//            $shopping_table = CashbackShopping::where('user_id', Auth::user()->user_id)->first();
-//
-//            $success =  $order->create([
-//                'order_code' => $order_code,
-//                'user_id' => Auth::user()->user_id,
-//                'username' => $request->username,
-//                'email' => $request->email,
-//                'address' => $request->address,
-//                'concact' => $request->contact,
-//                'sum' => $price,
-//                'products' => $request->product_id,
-//                'delivery_id' => $request->delivery
-//            ]);
-//        }
-        dd($request->all());
 
     }
 }
