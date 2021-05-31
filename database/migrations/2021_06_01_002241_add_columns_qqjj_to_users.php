3bfdf2c7d56407e7cf51e8e0c8e5f61aba571849
dauletjspro <15-35-1kosai-24->
2021-06-01 00:23:05 +0600

    fix bugs

--- database/migrations/2021_06_01_002241_add_columns_qqjj_to_users.php
+++ database/migrations/2021_06_01_002241_add_columns_qqjj_to_users.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddColumQQJJToUsers extends Migration
+class AddColumnsQqjjToUsers extends Migration
 {
     /**
      * Run the migrations.
