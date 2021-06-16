49b43951be96159eb8a8994df847f9fcd3170809
dauletjspro <15-35-1kosai-24->
2021-06-16 14:03:39 +0600

    fix bug

--- app/Http/Controllers/Admin/ShopController.php
+++ app/Http/Controllers/Admin/ShopController.php
@@ -26,7 +26,6 @@ class ShopController extends Controller
     public function index(Request $request)
     {
         $request->packet = Packet::where('is_show', 1)
-            ->where('is_old', 0)
             ->whereIn('packet_id', array_merge([Packet::CLASSIC, Packet::PREMIUM, Packet::VIP2, Packet::GAPTechno, Packet::GAPAuto, Packet::GAPHome], Packet::actualPassivePackets(), Packet::actualEntrepreneurPackets()))
             ->orderBy('packet_id', 'asc')
             ->select('*',
